f25651705ee058badb5b754ae6e8419508f103c6
Hazurl <ricoujules69@gmail.com>
2021-02-28 04:15:01 +0100

    Add job_c concept

---
+++ include/cppexp/job.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <functional>
+
+namespace cppexp {
+
+template<typename return_type>
+auto execute_job_sync(std::function<return_type()> const& job) 
+    -> void
+{
+    job();
+}
+
+template<typename return_type>
+auto execute_job_sync(return_type (*job) ()) 
+    -> void
+{
+    job();
+}
+
+template<typename job_type>
+concept job_c = requires(job_type j) 
+{
+    { ::cppexp::execute_job_sync(j) };
+};
+
+
+}
