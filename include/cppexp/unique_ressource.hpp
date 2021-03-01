45097f51fa872bcbee40fe83e36f6af48e35aa0c
Hazurl <ricoujules69@gmail.com>
2021-03-01 02:49:32 +0100

    Add operator* and operator-> to unique_ressource

--- include/cppexp/unique_ressource.hpp
+++ include/cppexp/unique_ressource.hpp
@@ -86,6 +86,18 @@ public:
         return deleter;
     }
 
+    auto operator*() const noexcept
+        -> resource_type const&
+    {
+        return get();
+    }
+
+    auto operator->() const noexcept
+        -> resource_type const*
+    {
+        return std::addressof(operator*());
+    }
+
 private:
 
     resource_type resource;
