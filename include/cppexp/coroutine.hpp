f72a9bbe0320a526cf6a022108c0608c5bea78ca
Hazurl <ricoujules69@gmail.com>
2021-03-01 02:47:38 +0100

    Fix make_unique_coroutine

--- include/cppexp/coroutine.hpp
+++ include/cppexp/coroutine.hpp
@@ -8,11 +8,18 @@
 namespace cppexp
 {
 
+
+template<typename promise_type = void>
+using unique_coroutine = unique_resource<
+    std::coroutine_handle<promise_type>, 
+    pure_deleter_t<std::coroutine_handle<promise_type>>>;
+
+
 template<typename promise_type = void>
 auto make_unique_coroutine(std::coroutine_handle<promise_type> handle)
-    -> unique_resource<std::coroutine_handle<promise_type>, pure_deleter_t<std::coroutine_handle<promise_type>>>
+    -> unique_coroutine<promise_type>
 {
-    return make_unique_resource_checked(handle, std::coroutine_handle<>{}, make_coroutine_deleter());
+    return make_unique_resource_checked(handle, std::coroutine_handle<promise_type>{}, make_coroutine_deleter<promise_type>());
 }
 
 }
